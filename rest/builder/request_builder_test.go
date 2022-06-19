433c42bacaceb861187bd2765859e89253d01fa2
facecrusher <felipe.bustamante.soto@gmail.com>
2022-06-19 01:20:27 -0400

    increase coverage

--- rest/builder/request_builder_test.go
+++ rest/builder/request_builder_test.go
@@ -3,6 +3,7 @@ package builder
 import (
 	"errors"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -10,10 +11,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
 func Test_DoPost(t *testing.T) {
 	defaultHeaders := make(http.Header)
 	defaultHeaders.Add("Content-Type", "application/json")
 	defaultTimeout := time.Duration(time.Second)
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("OK - Created"))
+	}))
+
 	rb := NewRequestBuilder(defaultHeaders, defaultTimeout)
 
 	type testCase struct {
@@ -24,6 +36,12 @@ func Test_DoPost(t *testing.T) {
 	}
 
 	testCases := []testCase{
+		{
+			name: "DoPost ok request",
+			url:  mockServer.URL,
+			body: `{"message":"test message"}`,
+			want: &response.ReqResponse{Response: &http.Response{StatusCode: http.StatusCreated}, ByteBody: []byte("OK - Created")},
+		},
 		{
 			name: "DoPost error wrong URL format",
 			url:  "test.com",
@@ -40,8 +58,13 @@ func Test_DoPost(t *testing.T) {
 
 	for _, tc := range testCases {
 		got := rb.DoPost(tc.url, tc.body)
-		if got.Err != nil {
+		if tc.want.Err != nil {
 			assert.Equal(t, tc.want.Err.Error(), got.Err.Error())
+		} else {
+			assert.NotNil(t, got)
+			assert.Nil(t, got.Err)
+			assert.Equal(t, tc.want.Response.StatusCode, got.StatusCode)
+			assert.Equal(t, tc.want.ByteBody, got.ByteBody)
 		}
 	}
 }
